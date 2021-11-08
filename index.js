0d629b026ceb1e30c682bbceb9370a8c9feaca30
Crypto Guy <57683659+TechInnovation-Blockchain@users.noreply.github.com>
2021-11-08 16:07:25 -0600

    fix sendinblue

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const sgMail = require('@sendgrid/mail');
 const cors = require('cors');
 
+const SibApiV3Sdk = require('sib-api-v3-sdk');
+const defaultClient = SibApiV3Sdk.ApiClient.instance;
+const apiKey = defaultClient.authentications['api-key'];
+apiKey.apiKey = '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';
+
 const app = express();
 const port = 3100;
 app.use(cors({
@@ -12,26 +17,52 @@ const api_key = "SG.rrxHzzCbT9KuDPdJQclvaQ.Ye966gg-RIeoPp45FxXgOG52Vda3sZh531U5O
 sgMail.setApiKey(api_key);
 app.get('/send_email', (req, res) => {
     const data = req.query.telegram_id;
-    const msg = {
-        to: ['newcaesar628@gmail.com'], // Change to your recipient
-        from: 'contact@autonio.foundation', // Change to your gem sender
-        subject: 'User Telegram ID to add to DAO chat',
-        text: 'User Telegram ID',
-        html: 'User Telegram ID : <strong>' + data.toString() + '</strong>',
-    };
-    (async () => {
-        try {
-            await sgMail.send(msg);
-            res.send({message: "success"});
-        } catch (error) {
-            console.error(error);
-        
-            if (error.response) {
-                console.error(error.response.body)
-            }
-            res.send({message: "error"});
-        }
-    })();
+    const address = req.query.account;
+    const staked = req.query.staked;
+
+    var apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+    var sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+    sendSmtpEmail = {
+        "sender":{
+           "email":"contact@autonio.foundation",
+           "name":"Autonio"
+        },
+        "subject":"This is my default subject line",
+        "templateId":17,
+        "params":{
+           "telegram_id" : "@" + data.toString(),
+           "address" : address.toString(),
+           "staked_amount" : staked.toString(),
+        },
+        "messageVersions":[
+          {
+              "to":[
+                {
+                    "email":"ikram@autonio.foundation",
+                    "name":"Ikram"
+                },
+                {
+                    "email":"rajat@autonio.foundation",
+                    "name":"Rajat"
+                },
+                {
+                    "email":"contact@autonio.foundation",
+                    "name":"Autonio"
+                },
+              ],
+              "params":{
+                 "greeting":"Welcome onboard!",
+                 "headline":"Be Ready for Takeoff."
+              },
+              "subject":"We are happy to be working with you"
+           },
+        ]
+     };
+    apiInstance.sendTransacEmail(sendSmtpEmail).then(function(data) {
+        res.send({message: "success"});
+    }, function(error) {
+        res.send({message: "error"});
+    });
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port::: ${port}!`))
